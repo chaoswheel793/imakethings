0a6f756b0b6bd9ec613077cff11abacb4991d930
chaoswheel793 <chris.scan777@gmail.com>
2025-11-18 11:09:49 -0800

    fix: utils.js with full delta + log (debug #3)

--- css/src/js/src/js/src/js/utils.js
+++ css/src/js/src/js/src/js/utils.js
@@ -1,1 +1,12 @@
-// Chaos Carver – Utility functions (math, save/load, device detection, etc.)
+export let lastTime = 0;
+export function getDeltaTime(currentTime) {
+  const delta = (currentTime - lastTime) / 1000;
+  lastTime = currentTime;
+  return Math.min(delta, 1/30);
+}
+
+export function log(...args) {
+  console.log('%c[I Make Things]', 'color: #ff3366; font-weight: bold;', ...args);
+}
+
+export const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
